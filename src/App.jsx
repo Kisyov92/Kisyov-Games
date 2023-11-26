1094a626910b09022270eddb8bd33517abc0c279
Atanas Kisyov <kisyov92@gmail.com>
2023-11-26 23:22:36 +0100

    configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Darts from "./games/darts/Darts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <p>Home Page</p> },
+      {
+        path: "darts",
+        element: <Darts />,
+      },
+      {
+        path: "simon",
+        element: <p>simon</p>,
+      },
+      {
+        path: "monkey-numbers",
+        element: <p>monkey-numbers</p>,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Kisyov-games</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
